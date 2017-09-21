a16156287d41d6a3bc0e1efe5b0efb845f8e9d69
bao liehua <baoliehua@gmail.com>
2017-09-21 13:15:02 +0800

    little change

---
+++ js/leaderboard.js
@@ -0,0 +1,19 @@
+ReactDOM.render(
+  <p>Points in past 30 days</p>,
+  document.getElementById("example")
+  );
+
+    
+  var Header = React.createClass({
+  render: function(){
+  return <h1>Leaderborder</h1>;
+  }
+  });
+
+  var Footer = React.createClass({
+      render:function(){
+        render:function(){
+          return <footer>footer</footer>;
+        }
+      }
+  });
